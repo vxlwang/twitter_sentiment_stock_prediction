c508355807f72646abef63983d76320cdfd9fb78
Vanessa Wang <vxlwang5@gmail.com>
2022-09-04 19:13:45 -0600

    Initial list of functions

---
+++ text_preprocessor.py
@@ -0,0 +1,14 @@
+def text_preprocess(data, content_col="Tweet content"):
+    """
+    Cleans the content of Tweets by removing URLs, user handles, and retweets
+
+    Parameters:
+    -----------
+    content_col: the column containing the text to clean
+    """
+    data["cleaned"] = data[content_col].replace(r"https?://[a-zA-Z0-9./]*", "", regex=True)
+    data["cleaned"] = data["cleaned"].replace(r"\s?RT\s*@[a-zA-z0-9]*:\s", "", regex=True)
+    data["cleaned"] = data["cleaned"].replace(r"(\s|^|\W)@[\w]*\)?", "", regex=True)
+    data["cleaned"] = data["cleaned"].replace(r"^\s\-\s", "", regex=True)
+    data["cleaned"] = data["cleaned"].replace(r"\s+", " ", regex=True)
+    data["cleaned"] = data["cleaned"].str.strip()
