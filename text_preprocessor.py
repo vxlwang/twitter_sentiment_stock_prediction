4a131a78ecd173f2bcf340b663a1118f139af025
Vanessa Wang <vxlwang5@gmail.com>
2022-09-16 01:17:58 -0600

    Added sentiment and regression functions

--- text_preprocessor.py
+++ text_preprocessor.py
@@ -1,16 +1,14 @@
-# imports
+import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import math
+from sklearn import metrics
 
-
-def text_preprocess(data, new_col_name="cleaned_content"):
+# Text Preprocess
+def text_preprocess(data, new_col_name="cleaned_text"):
     """
     Cleans the content of Tweets by removing URLs, user handles, and retweets, then appends a new column which contains
     the cleaned version of the text
-
-    :param data: Series or column containing the text to clean
-    :param new_col_name: name of the new column
     """
-
     data[new_col_name] = data.replace(r"https?://[a-zA-Z0-9./]*", "", regex=True)
     data[new_col_name] = data[new_col_name].replace(r"\s?RT\s*@[a-zA-z0-9]*:\s", "", regex=True) # retweets
     data[new_col_name] = data[new_col_name].replace(r"(\s|^|\W)@[\w]*\)?", "", regex=True) # user handles/mentions
@@ -19,15 +17,14 @@ def text_preprocess(data, new_col_name="cleaned_content"):
     data[new_col_name] = data[new_col_name].replace(r"\s+", " ", regex=True)
     data[new_col_name] = data[new_col_name].str.strip()
 
-def vader_sentiment(data):
+def vader_score(data):
     """
-    Get negative, positive, neutral scoring ranging from -1 (extreme negative) to +1 (extreme positive)
+    Get negative, positive, neutral scoring
     """
     vader_score = SentimentIntensityAnalyzer()
 
     scores = []
     for index in range(data.shape[0]):
-        # print(analyser.polarity_scores(sentiments_pd['text'][i]))
         compound = vader_score.polarity_scores(data[index])["compound"]
         pos = vader_score.polarity_scores(data[index])["pos"]
         neu = vader_score.polarity_scores(data[index])["neu"]
@@ -39,7 +36,48 @@ def vader_sentiment(data):
                        "Neutral": neu
                        })
 
-    # sentiments_score = pd.DataFrame.from_dict(scores)
-    # cleaned_text = tweets.join(sentiments_score)
+    sentiments_score = pd.DataFrame.from_dict(scores)
 
-    return scores
+    return sentiments_score
+
+def get_sentiment(series):
+    """
+    classify VADER polarity scores into positive, neutral, and negative moods
+    """
+    sentiment = []
+    for score in series:
+        if score >= 0.05:
+            sentiment.append("Positive")
+        elif score <= -0.05:
+            sentiment.append("Negative")
+        else:
+            sentiment.append("Neutral")
+
+    series["Sentiment"] = sentiment
+
+    return series
+
+# Regression
+def regression_metrics(y_true, yhat):
+    """
+    return R^2, MSE, MAE, and RMSE metrics
+    """
+
+    print(f"R^2: {metrics.r2_score(y_true, yhat)}")
+    print(f"Mean Squared Error: {metrics.mean_squared_error(y_true, yhat)}")
+    print(f"Mean Absolute Error: {metrics.mean_absolute_error(y_true, yhat)}")
+    print(f"Root MSE: {metrics.mean_squared_error(y_true, yhat, squared=False)}")
+    print(f"% MAE: {metrics.mean_absolute_percentage_error(y_true, yhat)}")
+
+# Modeling and Predictions
+def time_series_split(data, train_size=0.8):
+    """
+    splits data while preserving order
+    """
+    split = math.ceil(len(data) * train_size)
+
+    train = data[:split]
+    test = data[split:]
+
+    return train, test
+
